8f163a7426b91c17abb5d297dd32c5e86a59893b
Kevin Karocyt <32278309+Karocyt@users.noreply.github.com>
2024-03-03 17:18:02 +0100

    test swipper

--- script.js
+++ script.js
@@ -3,16 +3,27 @@ document.addEventListener('DOMContentLoaded', function() {
     // loadPhotos(); // Implémentez cette fonction si vous souhaitez charger des photos dynamiquement
 });
 
+// Fonction pour charger les vidéos YouTube dans le carrousel
 function loadYouTubeVideos(videoIDs) {
-    const videoGallery = document.getElementById('video-gallery');
+    const videoGallery = document.querySelector('.video-swiper .swiper-wrapper');
     videoIDs.forEach(id => {
+        const slide = document.createElement('div');
+        slide.className = 'swiper-slide';
         const iframe = document.createElement('iframe');
         iframe.setAttribute('src', `https://www.youtube.com/embed/${id}`);
-        iframe.setAttribute('width', '560');
-        iframe.setAttribute('height', '315');
         iframe.setAttribute('frameborder', '0');
         iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
         iframe.setAttribute('allowfullscreen', true);
-        videoGallery.appendChild(iframe);
+        slide.appendChild(iframe);
+        videoGallery.appendChild(slide);
+    });
+    // Initialisez le carrousel des vidéos après avoir ajouté les vidéos
+    new Swiper('.video-swiper', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        spaceBetween: 30,
     });
 }
