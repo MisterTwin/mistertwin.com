8e5ffbedb3f353ccb4789c0c0e9bb44d58171a05
Kevin Karocyt <32278309+Karocyt@users.noreply.github.com>
2024-03-03 16:26:55 +0100

    reboot vanilla

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+    loadYouTubeVideos();
+    // loadPhotos(); // Implémentez cette fonction si vous souhaitez charger des photos dynamiquement
+});
+
+function loadYouTubeVideos() {
+    const videos = ["YOUR_VIDEO_ID_1", "YOUR_VIDEO_ID_2
