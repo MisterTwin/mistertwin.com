dbe8ed1e8317bfbaed901b7920d67792928d34b8
Kevin Karocyt <32278309+Karocyt@users.noreply.github.com>
2024-03-03 16:43:55 +0100

    test youtube 3

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    loadYouTubeVideos();
+    loadYouTubeVideos(["04gT1eFESpk", "Ld1AghHNKxA", "QJ5m2YRwtyQ"]);
     // loadPhotos(); // Implémentez cette fonction si vous souhaitez charger des photos dynamiquement
 });
 
-function loadYouTubeVideos() {
-    const videos = ["04gT1eFESpk", "Ld1AghHNKxA", "QJ5m2YRwtyQ"];
+function loadYouTubeVideos(videoIDs) {
+    const videoGallery = document.getElementById('video-gallery');
+    videoIDs.forEach(id => {
+        const iframe = document.createElement('iframe');
+        iframe.setAttribute('src', `https://www.youtube.com/embed/${id}`);
+        iframe.setAttribute('width', '560');
+        iframe.setAttribute('height', '315');
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allow', 'accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture');
+        iframe.setAttribute('allowfullscreen', true);
+        videoGallery.appendChild(iframe);
+    });
 }
