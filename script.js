4d52111047e736dee5e61c8b071e924cfe92b4ce
Kevin Karocyt <32278309+Karocyt@users.noreply.github.com>
2024-03-08 17:01:48 +0100

    contact form with emailJS

--- script.js
+++ script.js
@@ -4,47 +4,52 @@ const nameInput = document.getElementById('name');
 const emailInput = document.getElementById('email');
 const messageInput = document.getElementById('message');
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault();
+(function() {
+    // https://dashboard.emailjs.com/admin/account
+    emailjs.init({
+      publicKey: "dE-B1ad6w32htDiJB",
+    });
+})();
 
-//     // Perform form validation
-//     if (nameInput.value.trim() === '') {
-//         alert('Please enter your name.');
-//         return;
-//     }
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
 
-//     if (emailInput.value.trim() === '') {
-//         alert('Please enter your email address.');
-//         return;
-//     }
+    // Perform form validation
+    if (nameInput.value.trim() === '') {
+        alert('Please enter your name.');
+        return;
+    }
 
-//     if (messageInput.value.trim() === '') {
-//         alert('Please enter a message.');
-//         return;
-//     }
+    if (emailInput.value.trim() === '') {
+        alert('Please enter your email address.');
+        return;
+    }
 
-//     // Send the form data to the server (replace this with your server-side code)
-//     const formData = new FormData(form);
-//     fetch('https://formspree.io/f/mjvnowvp', {
-//         method: 'POST',
-//         body: formData,
-//         headers: {
-//             'Accept': 'application/json'
-//         }
-//     })
-//     .then(response => {
-//         if (response.ok) {
-//             alert('Votre message a bien été envoyé. Merci !');
-//             form.reset();
-//         } else {
-//             alert('Une erreur s\'est produite lors de l\'envoi de votre message. Veuillez réessayer plus tard.');
-//         }
-//     })
-//     .catch(error => {
-//         alert('Une erreur s\'est produite lors de l\'envoi de votre message. Veuillez réessayer plus tard.');
-//         console.error('Error:', error);
-//     });
-// });
+    if (messageInput.value.trim() === '') {
+        alert('Please enter a message.');
+        return;
+    }
+
+    // Send the form data to the server (replace this with your server-side code)
+    // const formData = new FormData(form);
+    emailjs.send("service_bduhepx","template_zpfyhre",{
+            from_name: nameInput.value,
+            message: messageInput.value,
+            reply_to: emailInput.value,
+    })
+    .then(response => {
+        if (response.ok) {
+            alert('Votre message a bien été envoyé. Merci !');
+            form.reset();
+        } else {
+            alert('Une erreur s\'est produite lors de l\'envoi de votre message. Veuillez réessayer plus tard.');
+        }
+    })
+    .catch(error => {
+        alert('Une erreur s\'est produite lors de l\'envoi de votre message. Veuillez réessayer plus tard.');
+        console.error('Error:', error);
+    });
+});
 
 // Hamburger menu:
 const toggleNav = document.querySelector('.toggle-nav');
@@ -79,3 +84,7 @@ document.addEventListener('click', (event) => {
         header.classList.remove('header-hidden');
     }
 });
+
+(function(){
+    emailjs.init("user_ID"); // Remplacez "user_ID" par votre propre User ID d'EmailJS
+  })()
